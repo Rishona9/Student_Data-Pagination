1c255aca690af124e328148ec55fa93525c7046a
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-08-07 13:19:32 -0400

    Created showPage function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+//Create the `showPage` function
+//This function will create and insert/append the elements needed to display a "page" of nine students
 
+function showPage(list, page) {
+  // create two variables which will represent the index for the first and last student on the page
+  const startIndex = page * 8 - 8;
+  const endIndex = page * 8;
+  // select the element with a class of `student-list` and assign it to a variable
+  const studentList = document.querySelector(".student-list");
+  // set the innerHTML property of the variable you just created to an empty string
+  studentList.innerHTML = "";
+  let studentItem = "";
+  // loop over the length of the `list` parameter
+  for (let i = 0; i < studentList.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      studentItem = +`
+    <li class="student-item cf">
+    <div class="student-details">
+      <img class="avatar" src=${list[i].picture.medium} alt="Profile Picture">
+      <h3>${list[i].name.first} ${list[i].name.last}</h3>
+      <span class="email">${list[i].email}</span>
+    </div>
+    <div class="joined-details">
+      <span class="date">${list[i].registered.date}</span>
+    </div>
+  </li>`;
+    }
+  }
+  // inside the loop create a conditional to display the proper students
+  // inside the conditional:
+  // create the elements needed to display the student information
+  // insert the above elements
+  studentList.insertAdjacentHTML("beforeend", studentItem);
+}
+showPage(data, 1);
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
+//Create the `addPagination` function
+//This function will create and insert/append the elements needed for the pagination buttons
+//*/
 
 // Call functions
