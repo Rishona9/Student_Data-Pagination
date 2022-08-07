da84e2e1e114f157f2bc444a3cc153aa94c34faa
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-08-07 16:22:03 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
-//Create the `showPage` function
 //This function will create and insert/append the elements needed to display a "page" of nine students
 
 function showPage(list, page) {
-  // create two variables which will represent the index for the first and last student on the page
   const startIndex = page * 9 - 9;
   const endIndex = page * 9;
-  // select the element with a class of `student-list` and assign it to a variable
   const studentList = document.querySelector(".student-list");
-  // set the innerHTML property of the variable you just created to an empty string
+
   studentList.innerHTML = "";
   let studentItem = "";
-  // loop over the length of the `list` parameter
+
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex && i < endIndex) {
       studentItem += `
@@ -26,10 +23,6 @@ function showPage(list, page) {
   </li>`;
     }
   }
-  // inside the loop create a conditional to display the proper students
-  // inside the conditional:
-  // create the elements needed to display the student information
-  // insert the above elements
   studentList.insertAdjacentHTML("beforeend", studentItem);
 }
 showPage(data, 1);
