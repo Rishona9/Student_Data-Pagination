d32d56aea22055dd00136bb11d618a613d0816b9
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-08-11 12:19:13 -0400

    addPagination function

--- js/script.js
+++ js/script.js
@@ -30,5 +30,32 @@ showPage(data, 1);
 //Create the `addPagination` function
 //This function will create and insert/append the elements needed for the pagination buttons
 //*/
+function addPagination(list) {
+  const numOfPages = Math.ceil(list.length / 9);
+  const linkList = document.querySelector(".link-list");
 
-// Call functions
+  linkList.innerHTML = "";
+  let button = "";
+
+  for (let i = 1; i <= numOfPages; i++) {
+    button += `<li>
+  <button type="button">${[i]}</button></li>`;
+  }
+  linkList.insertAdjacentHTML("beforeend", button);
+
+  let buttonActive = document.querySelector("button");
+  buttonActive.className = "active";
+
+  linkList.addEventListener("click", (e) => {
+    const buttonClicked = e.target;
+    if (buttonClicked.tagName === "BUTTON") {
+      const activeClassButton = document.querySelector(".active");
+      activeClassButton[0].className = "";
+      buttonClicked.className = "active";
+      showPage(list, buttonClicked.textContent);
+    }
+  });
+}
+
+showPage(data, 1);
+addPagination(data);
